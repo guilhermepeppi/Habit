87bcb516d4c122f4f1426d2ad4ec98fbb3f4eb2d
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-08-01 11:24:38 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Guilherme Peppi on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
