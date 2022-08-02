1f21d1754c805664b53a6474e58f818ee2732d55
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-08-02 11:01:06 -0300

    First upload

---
+++ Habit/Splash/Model/SplashUIState.swift
@@ -0,0 +1,15 @@
+//
+//  SplashUIState.swift
+//  Habit
+//
+//  Created by Guilherme Peppi on 01/08/22.
+//
+
+import SwiftUI
+
+enum SplashUIState {
+    case loading
+    case goToSignInScreen
+    case goToHomeScreen
+    case error(String)
+}
